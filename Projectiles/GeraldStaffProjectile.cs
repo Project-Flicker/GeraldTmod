3d72d5067dc9af2c6a3e6d47d847aaa40b78e1e9
ZacIsBurnt <65946597+ZacIsBurnt@users.noreply.github.com>
2020-09-17 13:10:13 +1000

    Create GeraldStaffProjectile.cs

---
+++ Projectiles/GeraldStaffProjectile.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace GeraldTmod.Projectiles
+{
+	public class GeraldStaffProjectile : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			projectile.width = 16;
+			projectile.height = 4;
+			projectile.magic = true;
+			projectile.friendly = true;
+			projectile.hostile = false;
+			projectile.penetrate = 2;
+			projectile.ai[0] = 0;
+			projectile.timeLeft = 600;
+		}
+
+		public override void AI() {
+			projectile.damage = 8;
+			if (projectile.position.X>Main.MouseWorld.X+1 || projectile.position.X<Main.MouseWorld.X-1) {
+				if (Main.MouseWorld.X<projectile.position.X)
+					projectile.velocity.X=-2f;
+				if (Main.MouseWorld.X>projectile.position.X)
+					projectile.velocity.X=2f;
+			}
+			if (projectile.position.Y>Main.MouseWorld.Y+1 || projectile.position.Y<Main.MouseWorld.Y-1) {
+				if (Main.MouseWorld.Y<projectile.position.Y)
+					projectile.velocity.Y=-2f;
+				if (Main.MouseWorld.Y>projectile.position.Y)
+					projectile.velocity.Y=2f;
+			}
+			if (projectile.position.Y>Main.MouseWorld.Y-1 && projectile.position.Y<Main.MouseWorld.Y+1)
+				projectile.velocity.Y=0f;
+			if (projectile.position.X>Main.MouseWorld.X-1 && projectile.position.X<Main.MouseWorld.X+1)
+				projectile.velocity.X=0f;
+			projectile.rotation = projectile.velocity.ToRotation();
+		}
+	}
+}
